fe2f6e29d60fc6f3491793963714662154af2a1c
1014698129@qq.com <6017825401a>
2018-12-18 17:57:31 +0800

    搭建SpringBoot框架以及关于Login的Demo小测试

---
+++ src/main/java/cn/edu/zzuli/controller/SignUpController.java
@@ -0,0 +1,11 @@
+package cn.edu.zzuli.controller;
+
+/**
+ * @ClassName SignUpController
+ * @Description 注册以及注销的相关操作
+ * @Author 任耀
+ * @Date 2018/12/18 17:52
+ * @Version 1.0
+ */
+public class SignUpController {
+}
